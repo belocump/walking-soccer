c8ba85630cf0241bc38859004027b2a3d6d767de
Belo <barazyuuzi2000@gmail.com>
2023-05-28 06:04:19 +0900

    タイトルの字を修正する #7

--- components/logo.js
+++ components/logo.js
@@ -3,7 +3,7 @@ export default function Logo() {
   return (
     <div className="mx-auto px-4 py-4 justify-center text-base flex container">
       <GiTurtle size="10rem" />
-      <h1 className="font-fancy1 text-8xl font-medium text-center mb-5">
+      <h1 className="font-fancy1 text-6xl font-medium text-center mb-5">
         Tortuga FC
       </h1>
       <GiTurtle size="10rem" />
