c43c4ea5b3ea61aede88d351865d85082f25958f
belocump <barazyuuzi2000@gmail.com>
2023-05-29 14:35:43 +0900

    詳細ページのスタイルを整える #8

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -48,7 +48,7 @@ const Post = ({ post }: any) => {
   // 目次ここまで
 
   return (
-    <section className="container h-auto lg:px-2 px-5 lg:w-3/5 mx-auto mt-20">
+    <section className="container h-auto lg:px-2 px-5 lg:w-4/5 mx-auto mt-20">
       {/* 目次 */}
       <div className="p-3 m-10 bg-gray-100 border border-black border-dashed">
         <p className="text-center text-3xl">目次</p>
@@ -65,50 +65,59 @@ const Post = ({ post }: any) => {
       </div>
       {/* 目次ここまで */}
 
-      <h2 className="w-full text-3xl font-medium">{post.metadata.title}</h2>
-      <div className="border-b-2 w-1/3 mt-1 border-sky-900"></div>
-      <span className="text-gray-500">Posted date at {post.metadata.date}</span>
-      <br />
-      {post.metadata.tags.map((tag: string, index: number) => (
-        <p
-          className="text-white bg-sky-900 rounded-xl font-medium mt-2 px-2 inline-block mr-2"
-          key={index}
-        >
-          <Link href={`/posts/tag/${tag}/page/1`}>{tag}</Link>
-        </p>
-      ))}
-      <div className="mt-10 font-medium markdown">
-        <ReactMarkdown
-          components={{
-            h2: H2,
-            code({ node, inline, className, children }) {
-              const match = /language-(\w+)/.exec(className || "");
-              return !inline && match ? (
-                <SyntaxHighlighter
-                  style={vscDarkPlus}
-                  language={match[1]}
-                  PreTag="div"
-                >
-                  {String(children).replace(/\n$/, "")}
-                </SyntaxHighlighter>
-              ) : (
-                <code>{children}</code>
-              );
-            },
-          }}
-        >
-          {post.markdown}
-        </ReactMarkdown>
+      {/* 詳細 */}
+      <div className="p-3 m-10 bg-gray-100 border border-black border-dashed">
+        <h2 className="text-3xl font-medium">{post.metadata.title}</h2>
 
-        <h2 className="text-white bg-sky-50 rounded-xl font-medium mt-2 px-2 inline-block mr-2">
-          <Link href={`/books/info/${post.metadata.slug}/`}>
-            ご予約はこちらから
-          </Link>
-        </h2>
-
+        <span className="text-gray-500">
+          Posted date at {post.metadata.date}
+        </span>
         <br />
+        {post.metadata.tags.map((tag: string, index: number) => (
+          <p
+            className="text-white bg-sky-900 rounded-xl font-medium mt-2 px-2 inline-block mr-2"
+            key={index}
+          >
+            <Link href={`/posts/tag/${tag}/page/1`}>{tag}</Link>
+          </p>
+        ))}
+        <div className="mt-10 font-medium markdown">
+          <ReactMarkdown
+            components={{
+              h2: H2,
+              code({ node, inline, className, children }) {
+                const match = /language-(\w+)/.exec(className || "");
+                return !inline && match ? (
+                  <SyntaxHighlighter
+                    style={vscDarkPlus}
+                    language={match[1]}
+                    PreTag="div"
+                  >
+                    {String(children).replace(/\n$/, "")}
+                  </SyntaxHighlighter>
+                ) : (
+                  <code>{children}</code>
+                );
+              },
+            }}
+          >
+            {post.markdown}
+          </ReactMarkdown>
+        </div>
+
+        <div className="container py-10 px-10 mx-0 min-w-full flex flex-col items-center">
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            type="submit"
+          >
+            <Link href={`/books/info/${post.metadata.slug}/`}>
+              ご予約はこちらから
+            </Link>
+          </button>
+        </div>
+
         <Link href="/">
-          <div className="pb-20 mt-10 text-sky-900 float-right">
+          <div className="pb-20 mt-10 text-sky-900 float-right text-2xl m-4">
             ←ホームに戻る
           </div>
         </Link>
